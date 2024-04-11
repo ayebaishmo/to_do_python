a5760528e5a072b780d1e67d189edbe5393a1aeb
ayebaishmo <ishmo255super@gmail.com>
2024-04-11 17:30:58 +0300

    Used a class and added delete function

--- store_todos.py
+++ store_todos.py
@@ -1,13 +1,31 @@
-# stores the todo
-todo = []
+class Todo_list:
+    def __init__(self):
+        self.todos = []
 
-#ceates the todos
-# todo.append('Eat lunch')
+    def create_todo(self, todo):
+        self.todos.append(todo)
+        return self.todos
+    
+    # def del_todo(self, todo_del):
+    #     self.todos.remove(todo_del)
+    #     return self.todos
+    
+    # delete by using index
+    def del_todo(self, todo_del):
+        if todo_del in self.todos:
+            index_tobe = self.todos.index(todo_del)
+            self.todos.pop(index_tobe)
+        return self.todos        
+    
+    def __repr__(self):
+        return str(self.todos)
 
-# print(todo)
-def create_todo(todo_one):
-    todo.append(todo_one)
-    return todo
-
-print(create_todo('Praise God'))
+if __name__=='__main__':
+    my_list = Todo_list()
+    To_do_one = my_list.create_todo('Praise God')
+    To_do_two = my_list.create_todo('Worship Him')
+    To_do_three = my_list.create_todo('Enjoy His Presence')
+    To_do_four = my_list.create_todo('Do somethings')
+    delete_one = my_list.del_todo('Do somethings')
+    print(delete_one)
 
